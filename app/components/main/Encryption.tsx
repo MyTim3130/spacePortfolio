3e967b757f3567cbe0ace3795b9fb4786c40f513
MyTim3130 <tin.hauslkramper@gmail.com>
2024-01-05 18:28:08 +0100

    v4 fix

--- app/components/main/Encryption.tsx
+++ app/components/main/Encryption.tsx
@@ -11,7 +11,7 @@ const Encryption = () => {
           variants={slideInFromTop}
           className="text-[40px] font-medium text-center text-gray-200"
         >
-          Intellegence
+          Intelligence
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
             {" "}
             &{" "}
